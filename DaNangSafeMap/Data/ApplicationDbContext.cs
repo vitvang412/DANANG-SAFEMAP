a8ed9ee9bf6946e8b1f836bae8419b60a75988c6
vitvang412 <vitvang412@gmail.com>
2026-02-14 00:53:37 +0700

    Save current work before switching branch

--- DaNangSafeMap/Data/ApplicationDbContext.cs
+++ DaNangSafeMap/Data/ApplicationDbContext.cs
@@ -1,1 +1,17 @@
 // TODO: Implement ApplicationDbContext
+using DaNangSafeMap.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaNangSafeMap.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<UserProfile> UserProfiles { get; set; } = null!;
+    }
+}
